20800c082690f3f70d8bcc467aacdd9f39d16b82
Charlon Palacay <charlon@gmail.com>
2019-10-24 15:07:49 -0700

    Delete init py files and update index template.

--- app_name/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .course_title import CourseTitle
-from .prerequisite import Prerequisite
-from .currics import CurricTitles
-from .graph import CourseGraph, CurricGraph
--- app_name/urls.py
+++ app_name/urls.py
@@ -1,16 +1,8 @@
 from django.urls import include, path, re_path
 from django.views.generic.base import RedirectView
-from prereq_map.views.pages import PageView
-from prereq_map.views.api import CurricApiView, CourseApiView, \
-    CurricTypeaheadApiView, CourseTypeaheadApiView
+from {{ app_name }}.views.pages import PageView
+
 
 urlpatterns = [
-    # path('', RedirectView.as_view(url='/curriculum-search/')),
-    # path('curriculum-search/', CurriculumSearch.as_view()),
-    # path('course-search/', CourseSearch.as_view()),
-    re_path(r'^api/curric/(?P<curric_code>.*)', CurricApiView.as_view()),
-    re_path(r'^api/course/(?P<course_code>.*)', CourseApiView.as_view()),
-    re_path(r'^api/curric_typeahead', CurricTypeaheadApiView.as_view()),
-    re_path(r'^api/course_typeahead', CourseTypeaheadApiView.as_view()),
     re_path(r'^.*$', PageView.as_view(), name='index')
 ]
