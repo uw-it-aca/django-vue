04789a2892973b840670aa512a00c8c0f431cf7a
Charlon Palacay <charlon@gmail.com>
2021-05-06 10:19:20 -0700

    Updates home.vue

--- app_name_vue/pages/home.vue
+++ app_name_vue/pages/home.vue
@@ -19,14 +19,38 @@
     <template #content>
       <div class="row">
         <div class="col-md-6">
-          <h2>Development</h2>
+          <h2>Development Stack</h2>
           <p>
             What's included? This template repository is intended to start you off with AXDD's
             supported development stack.
           </p>
           <ul>
-            <li>Django Container</li>
-            <li>BootstrapVue (BS 4)</li>
+            <li>Django (2.1 - 2.3)
+            <li>Vue (2.x)</li>
+            <li>Webpack (5.x)</li>
+            <li>Bootstrap (4.6.x)</li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <h2>AXDD Libraries</h2>
+          <p>
+           The following is a list of specific AXDD libraries used in building applications.
+          </p>
+          <ul>
+            <li><a href="https://github.com/uw-it-aca/django-container" target="_blank">Django Container</a></li>
+            <li><a href="https://github.com/uw-it-aca/django-webpack-bridge" target="_blank">Django Webpack Bridge</a></li>
+            <li><a href="https://github.com/uw-it-aca/axdd-components" target="_blank">AXDD Components</a></li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <h2>Vue Libraries</h2>
+          <p>
+           The following is a list of common Vue libraries included in this repository.
+          </p>
+          <ul>
+            <li><a href="https://bootstrap-vue.org/" target="_blank">BootstraVue</a></li>
+            <li><a href="https://github.com/FortAwesome/vue-fontawesome" target="_blank">Vue FontAwesome</a></li>
+            <li><a href="https://matteo-gabriele.gitbook.io/vue-gtag/" target="_blank">Vue Gtag</a></li>
           </ul>
         </div>
         <div class="col-md-6">
