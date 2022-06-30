eaf4fc13686ccc037a2241209d1d1e3018faf8de
Craig M. Stimmel <cstimmel@uw.edu>
2022-06-30 14:45:13 -0700

    Use getattr with default that should work in most cases

--- app_name/templatetags/vite.py
+++ app_name/templatetags/vite.py
@@ -13,7 +13,7 @@ register = template.Library()
 def vite_manifest(entries_names):
 
     # path to the manifest.json (relative if localdev, /static if not)
-    manifest_filepath = settings.VITE_MANIFEST_PATH
+    manifest_filepath = getattr(settings, 'VITE_MANIFEST_PATH', '/static/manifest.json')
 
     with open(manifest_filepath) as fp:
         manifest = json.load(fp)
