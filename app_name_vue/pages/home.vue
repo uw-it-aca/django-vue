28beb45fbe999aa366ebbb93c719aba1b4a46b59
Craig M. Stimmel <cstimmel@uw.edu>
2022-06-30 14:33:21 -0700

    Use django app statics in localdev, but /static in production

--- app_name_vue/pages/home.vue
+++ app_name_vue/pages/home.vue
@@ -11,7 +11,7 @@
       <div class="p-0 col-md-8 lead">
         <HelloWorld />
         <p>
-          ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget
           pulvinar est. Sed gravida neque justo, pellentesque finibus velit
           scelerisque eu. Curabitur et dolor luctus, convallis magna at, mollis
           purus. Nunc tincidunt risus quis nisl bibendum volutpat. Praesent ut
