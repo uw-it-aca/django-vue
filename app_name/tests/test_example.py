78908febecf94ce41e72cce90ccd24a900e3f75f
Craig M. Stimmel <cstimmel@uw.edu>
2022-10-05 13:56:01 -0700

    Add a test for the vite templatetag

--- app_name/tests/test_example.py
+++ app_name/tests/test_example.py
@@ -1,7 +1,9 @@
 # Copyright 2022 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
+import re
 from django.test import TestCase
+from app_name.templatetags.vite import vite_styles
 
 
 class YourTestClass(TestCase):
@@ -16,6 +18,11 @@ class YourTestClass(TestCase):
     def test_something_that_will_pass(self):
         self.assertFalse(False)
 
-
 #    def test_something_that_will_fail(self):
 #        self.assertTrue(False)
+
+    def test_templatetag_vite_styles(self):
+        entries = ('app_name_vue/main.js',)
+        link = vite_styles(*entries)
+        pattern = re.compile("<link rel=\"stylesheet\" href=\"/static/app_name/assets/main\.[\d\w]*\.css\" />")
+        self.assertTrue(pattern.match(link))
