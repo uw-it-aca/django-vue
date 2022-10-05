39c2fe350b3e22d1019fec20993386312021420b
Charlon Palacay <charlon@gmail.com>
2022-10-05 14:32:07 -0700

    Creates separate test. Fixes pep8.

--- app_name/tests/test_example.py
+++ app_name/tests/test_example.py
@@ -3,7 +3,6 @@
 
 import re
 from django.test import TestCase
-from app_name.templatetags.vite import vite_styles
 
 
 class YourTestClass(TestCase):
@@ -20,9 +19,3 @@ class YourTestClass(TestCase):
 
 #    def test_something_that_will_fail(self):
 #        self.assertTrue(False)
-
-    def test_templatetag_vite_styles(self):
-        entries = ('app_name_vue/main.js',)
-        link = vite_styles(*entries)
-        pattern = re.compile("<link rel=\"stylesheet\" href=\"/static/app_name/assets/main\.[\d\w]*\.css\" />")
-        self.assertTrue(pattern.match(link))
