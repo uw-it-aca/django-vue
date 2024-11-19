e8925b9034de35aca3e1688eaad156c509090f45
Craig M. Stimmel <cstimmel@uw.edu>
2024-11-19 13:29:11 -0800

    coverage currently causing the build to fail.

--- app_name/apps.py
+++ app_name/apps.py
@@ -2,7 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from django.apps import AppConfig
+#from django.contrib.staticfiles.apps import StaticFilesConfig
 
+
+#class MyStaticFilesConfig(StaticFilesConfig):
+#    ignore_patterns = ['CVS', '*~']
 
 class AppNameConfig(AppConfig):
     name = "app_name"
