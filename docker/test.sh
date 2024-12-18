08298592ac3c81c1d2cdd888eccf3cc7c194a151
Charlon Palacay <charlon@gmail.com>
2024-12-18 08:20:28 -0800

    Install coverage in virtualenv.

--- docker/test.sh
+++ docker/test.sh
@@ -7,6 +7,9 @@ trap 'exit 1' ERR
 # start virtualenv
 source bin/activate
 
+# force install coverage in virtualenv
+pip install coverage
+
 function run_test {
     echo "##########################"
     echo "TEST: $1"
