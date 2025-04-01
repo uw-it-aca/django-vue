c8d07102461a6439f5049dbb7b2488f32e2e0596
Charlon Palacay <charlon@gmail.com>
2025-04-01 13:51:47 -0700

    Updates prettier and stylelint configs.

---
+++ stylelint.config.js
@@ -0,0 +1,23 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-standard-vue/scss',
+  ],
+  customSyntax: 'postcss-html',
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue'],
+      rules: {
+        'scss/no-global-function-names': null,
+        'no-invalid-position-at-import-rule': null,
+        'scss/at-import-partial-extension': [
+          'never',
+          {
+            severity: 'warning',
+          },
+        ],
+      },
+    },
+  ],
+}
