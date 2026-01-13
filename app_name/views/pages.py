77536b3c49f19bfa65d5bf030cdcfe6385488316
Charlon Palacay <charlon@gmail.com>
2026-01-13 13:37:02 -0800

    Get env variables via Django settings and docker.

--- app_name/views/pages.py
+++ app_name/views/pages.py
@@ -1,14 +1,22 @@
 # Copyright 2026 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
 class PageView(TemplateView):
     template_name = "index.html"
 
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response({"context_data": context})
+
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = {}
+        context["debugMode"] = settings.DEBUG
+        context["googleAnalyticsKey"] = settings.GOOGLE_ANALYTICS_KEY
+        context["clarityProjectId"] = settings.CLARITY_PROJECT_ID
         return context
 
 
